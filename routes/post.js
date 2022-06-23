4eef151071a7814c780cb643a22f7dd168d6278f
cesar2485 <cesmartinez@outlook.com>
2022-06-23 17:16:33 -0500

    funcionamiento de API

---
+++ routes/post.js
@@ -0,0 +1,37 @@
+
+const { Router } = require('express');
+const router = Router();
+
+// const persons = require('./sample.json');
+
+router.get('/', (req, res) => {
+    res.json(
+
+        [
+            {
+                "id" : "16944838",
+                "nombre" : "Andres",
+                "Apellido" : "Castillo",
+                "e-mail" : "amaucas@gmail.com",
+                "tel" : "3218750729"
+            },
+            {
+                "id" : "123456789",
+                "nombre" : "Pepe",
+                "Apellido" : "Sanchez",
+                "e-mail" : "pepesanchez@gmail.com",
+                "tel" : "321123456789"
+            }
+        ]
+        
+    );
+})
+
+router.get('/', (req, res) => {
+    res.send(
+        "<h1>Hola mundo</h1>"
+    );
+});
+
+
+module.exports = router;
