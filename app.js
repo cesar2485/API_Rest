b50139ed4fe891634b5f4e5b0f69237c8551d9f0
cesar2485 <cesmartinez@outlook.com>
2022-06-23 09:45:07 -0500

    conexion de API con BD

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 
 app.get('/', (req, res) => {
    res.send('Primera pagina Web por API Rest');
 });
-
+mongoose.connect('mongodb+srv://cesar:27052007@cluster.rac3u.mongodb.net/?retryWrites=true&w=majority',
+{useNewUrlParser: true}, () => {
+   console.log('Si hay conexion a la BD');
+});
 app.listen(10000);
